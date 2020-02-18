2b4d036611799d23c92a4795179f4849d8af1d8e
Alexander Scherer <alexscherer@outlook.com>
2020-02-18 17:35:25 +0100

    Create React App and first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import Layout from './components/Layout';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Ubuntu', -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+      background-color: #CDCDCD;
+  }
+  p {
+    margin-top: 0;
+    font-size: (1em + 1vw); 
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  
+  a {
+    font-size: (1em + 1vw); 
+    }
+
+  h1 {
+    color: #FF0000;
+    margin: 15px;
+    text-align: center;
+    font-size: 2em; 
+    }
+
+  h2 {
+    color: black;
+    margin: 15px;
+    text-align: center;
+    font-size: 1.5em;
+    }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout />
+      <div>Test</div>
+    </>
   );
 }
 
